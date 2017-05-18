9341825249bf2035da23782d3554b6f8f2df7c59
fukayatsu <fukayatsu@gmail.com>
2017-05-18 11:29:09 +0900

    Add example config

---
+++ lita_config.rb
@@ -0,0 +1,13 @@
+Lita.configure do |config|
+  config.robot.name       = 'lita'
+  config.robot.adapter    = 'twitter'
+  config.robot.log_level  = :info
+
+  config.redis[:url] = 'redis://localhost:6379/0'
+  config.http.port   = 8080
+
+  config.adapters.twitter.api_key             = ENV['API_KEY']
+  config.adapters.twitter.api_secret          = ENV['API_SECRET']
+  config.adapters.twitter.access_token        = ENV['ACCESS_TOKEN']
+  config.adapters.twitter.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
